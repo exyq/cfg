5159387721e1f3beb45b643972d8c7e6e6d0fbea
exyq <ericxingy@163.com>
2023-03-06 01:07:21 +0800

    upload

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+# init
+chcp 65001
+clear
+
+# variable
+$pro = $profile
+$git = 'C:/Users/git'
+$vim = 'C:/Users/ericx/_vimrc'
+$c = 'C:/'
+$d = 'D:/'
+$e = 'E:/'
+$f = 'F:/'
+
+# function
+function cg {cd C:/Users/git}
+function pusha {C:/Users/git/push.bat}
+function reboot {shutdown -r -t 0}
+function shut {shutdown -s -t 0}
+
+#key
+C:/Users/ericx/Documents/Powershell/key.ps1
+
+
