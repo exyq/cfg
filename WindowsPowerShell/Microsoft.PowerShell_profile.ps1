a81c41020404c79de8431e5514d913ab84bd62c6
exyq <ericxingy@163.com>
2023-03-07 22:05:33 +0800

    upload

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,22 @@
+# init
+chcp 65001
+clear
+
+# variable
+$pro = $profile
+$git = 'C:/Users/git'
+$vim = '~/_vimrc'
+$c = 'C:/'
+$d = 'D:/'
+$e = 'E:/'
+$f = 'F:/'
+
+# function
+function cg {cd $git}
+function reboot {shutdown -r -t 0}
+function shut {shutdown -s -t 0}
+
+#key
+~/Documents/WindowsPowershell/key.ps1
+
+
