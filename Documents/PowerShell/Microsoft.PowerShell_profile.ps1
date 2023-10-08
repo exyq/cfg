1b630e981331e266e1004235f09c80a59c3c2fae
exyq <ericxingy@163.com>
2023-10-08 22:40:36 +0800

    upload

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,13 @@
 # init
 chcp 65001
 clear
-D:\Terminal\oh-my-posh\bin\oh-my-posh.exe init pwsh -c D:\Terminal\oh-my-posh\themes\montys.omp.json | invoke-expression
+D:/Terminal/oh-my-posh/bin/oh-my-posh.exe init pwsh -c D:/Terminal/oh-my-posh/themes/montys.omp.json | invoke-expression
 
 # variable
 $p = $profile
-$g = 'C:/git/'
-$VIM = 'C:/Program Files (x86)/Vim'
-$v = 'C:/Program Files (x86)/Vim/_vimrc'
+$g = 'D:/Terminal/git'
+$VIM = 'D:/Terminal/vim/'
+$v = 'D:/Terminal/vim/_vimrc'
 $c = 'C:/'
 $d = 'D:/'
 $e = 'E:/'
